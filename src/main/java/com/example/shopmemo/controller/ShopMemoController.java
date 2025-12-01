e9168bf4a6a1d7fb1dd70749d4cc8439f04380f3
Hisa840127 <day840127@yahoo.co.jp>
2025-12-01 22:14:46 +0900

    12/1

--- src/main/java/com/example/shopmemo/controller/ShopMemoController.java
+++ src/main/java/com/example/shopmemo/controller/ShopMemoController.java
@@ -1,7 +1,12 @@
 package com.example.shopmemo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.shopmemo.model.Item;
 
 @Controller
 public class ShopMemoController {
@@ -12,11 +17,23 @@ public class ShopMemoController {
         return "index"; // index.html
     }
 
-    // 買うものリスト画面
+ // 買うものリスト画面 
     @GetMapping("/list")
-    public String showList() {
-        return "list"; // list.html
+    public String showList(Model model) {
+
+        // Mockデータ（DBから来たフリのデータ）
+        List<Item> items = List.of(
+            new Item("卵", 10, "個", "残り少ない気がする"),
+            new Item("牛乳", 2, "本", "朝食用")
+        );
+
+        // HTML に渡すための詰め込み処理（昔の request.setAttribute に相当）
+        model.addAttribute("items", items);
+
+        // list.html を表示
+        return "list";
     }
+
 
     // 購入履歴画面
     @GetMapping("/history")
